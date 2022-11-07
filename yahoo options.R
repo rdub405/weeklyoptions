b7b8c33d02e1b6c820eb0d4cb0b02f491d29346c
Rashaw Williams <rashaww@gmail.com>
2022-11-07 15:29:28 -0600

    Update yahoo options.R

--- yahoo options.R
+++ yahoo options.R
@@ -2,8 +2,10 @@ library(rvest)
 library(tidyverse)
 library(dplyr)
 library(lubridate)
+library(tidyquant)
 
-#ticker <- c("UBER")
+#ticker <- c("PLTR")
+current_price <- getQuote("SOXL")
 
 calls <- paste0('https://finance.yahoo.com/quote/',ticker,'/options?p=',ticker,'')
 # Use read_html to fetch the webpage
@@ -37,9 +39,22 @@ df_calls <- df_calls %>%
 df_calls$symbol <- ticker
 df_calls$ExpDate <-ceiling_date(Sys.Date(), "week", week_start = getOption("lubridate.week.start", 1))-3
 
+#df_calls$Open_Option_Price <- (df_calls$Last_Price - df_calls$Change)
+#df_calls$Close_Option_Price <- (df_calls$Last_Price)
+#df_calls$Option_Change <- (df_calls$Open_Option_Price  - df_calls$Close_Option_Price)
+#df_calls$Change_Option_Price <- (current_price$Open - current_price$Last)
+#df_calls$Probability <- (df_calls$Change_Option_Price/df_calls$Option_Change)
+#df_calls$Odds <- 100-(100 * df_calls$Probability)
+  
 #final dataframe
 ticker_df <- df_calls %>%
-              select(symbol,Contract_Name, Strike,Last_Price, Volume, Open_Interest,ExpDate) %>%
-                filter(Strike >= round(level_2_strike_max))
-  
+              select(symbol,Contract_Name, Strike,Last_Price,Change,Volume, Open_Interest,ExpDate) 
 
+#final dataframe
+ticker_df <- df_calls %>%
+  select(symbol,Contract_Name, Strike,Last_Price, Change, Open_Option_Price, Close_Option_Price, Option_Change, Odds,Volume, Open_Interest,ExpDate) 
+
+
+#get single strike price               
+filter_df <- ticker_df %>%
+                filter(Strike == strike_price)
