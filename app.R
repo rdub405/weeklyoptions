2a46dab34c0ded683aadbb42f76008acf97013b9
Rashaw Williams <rashaww@gmail.com>
2023-12-06 15:05:06 -0600

    Update app.R

--- app.R
+++ app.R
@@ -1,22 +1,46 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
 
 library(shiny)
 
+# Define UI for application that draws a histogram
+ui <- fluidPage(
 
+    # Application title
+    titlePanel("Earnings Input"),
 
-# Define server logic ----
-server <- function(input, output) {
-  
-  output$plot <- renderPlot({
-    plot(sp)
-    
-  })
-  
-}
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+          textInput("Ticker", "Ticker", "BABA")
+        ),
 
-# Define UI ----
-ui <- basicPage(
-  plotOutput("plot")
+        # Show a plot of the generated distribution
+        mainPanel(
+          textOutput("greeting"),
+          tableOutput("dataTable"),
+          tableOutput("earningsTable")
+        )
+    )
 )
 
-# Run the app ----
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$greeting <- renderText({
+    paste("Symbol: ", input$Ticker)
+  })
+
+  output$dataTable <- renderTable(put_strike_output_df)
+  
+  output$earningsTable <- renderTable(earnings_options_df)
+}
+
+# Run the application 
 shinyApp(ui = ui, server = server)
