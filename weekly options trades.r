19af7103314896559743bbe858a6a52e02c536d7
Rashaw Williams <rashaww@gmail.com>
2024-01-04 15:17:32 -0600

    Update weekly options trades.r

--- weekly options trades.r
+++ weekly options trades.r
@@ -1,8 +1,9 @@
 library(tidyquant)
 library(dplyr)
+library(plotly)
 
 
-ticker <- c("BAC")
+ticker <- c("MSFT")
 current_price <- getQuote(ticker)
 
 prices <- tq_get(ticker,
@@ -23,11 +24,23 @@ weekly_change <- df_o_c %>%
     #,Weekly_Change = close - open)
 
 weekly_move <- weekly_change %>%
-  mutate(week_move = Close_Week_End - Open_Week_Start) %>%
-  filter(Week != 1)      
+  mutate(week_move = ((Close_Week_End - Open_Week_Start)/Open_Week_Start)*100) %>%
+  filter(Week != 1 & week_move < 0)      
 
 
 print(weekly_move)
 
 #Max % decease
-min(weekly_move$week_move)
+max_drop <- min(weekly_move$week_move)
+#boxplot(weekly_move$week_move, ylab="Percent Drop", xlab=ticker, horizontal=TRUE)
+
+#adding jittered point  https://plotly.com/r/box-plots/
+option_box <- plot_ly(y= weekly_move$week_move, type = "box", boxpoints = "all", jitter = 0.3,
+               pointpos = -1.8, name = ticker)
+option_box <- option_box %>% layout(title = "Percentage Drop")
+option_box
+
+
+
+
+
